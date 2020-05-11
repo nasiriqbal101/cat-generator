bbf20d345ebf4cca1f337f55d5c6c7f34a22a639
nasiriqbal101 <nasiriqbal101@yahoo.com>
2020-05-11 22:32:08 +0100

    js added and interaction works

---
+++ app.js
@@ -0,0 +1,9 @@
+ const generateCat = () => {
+
+    const image = document.createElement('img');
+    const div = document.getElementById('flex-cat-gen');
+    image.src = "https://cdn2.thecatapi.com/images/coq.jpg";
+    div.appendChild(image);
+
+
+ }
