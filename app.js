3493b0c3b651a84d2a4030a2b8f230bd934c9acd
nasiriqbal101 <nasiriqbal101@yahoo.com>
2020-05-13 22:12:13 +0100

    stlying done

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
- const generateCat = () => {
-
+const generateCat = () => {
     const image = document.createElement('img');
     const div = document.getElementById('flex-cat-gen');
+
     image.src = "https://cdn2.thecatapi.com/images/coq.jpg";
     div.appendChild(image);
-
-
- }
+}
